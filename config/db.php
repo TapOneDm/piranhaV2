278dccb88c8942458ed29b9c9290ac7cbc81e36a
TapOneDm <dmatskevich1@gmail.com>
2024-10-07 00:19:56 +0300

    updates final

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=host1862806',
-    'username' => 'host1862806',
-    'password' => 'hESzsR05fv',
+    'username' => 'admin',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
