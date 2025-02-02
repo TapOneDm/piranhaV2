bf2f5a21dd21135917ea52b78e4acd41b31d447b
dmitry_matskevich <dm@gmail.com>
2025-02-02 18:59:03 +0300

    fix

--- views/site/_modal-language.php
+++ views/site/_modal-language.php
@@ -4,7 +4,7 @@
         <div class="modal-body">
             <ul class="modal-language-lang-list">
                 <?php foreach (Yii::$app->translateManager->getLanguagesList() as $language) { ?>
-                    <li><a href="<?= $this->getLangUrl($language['code']) ?>" class="<?= $language['code'] === Yii::$app->language ? 'active' : null ?>" data-modal-open data-lang-choose="true"><img src="/static/img/<?=$language['code'].'-flag.png'?>" alt="<?=$language['title']?>"><span><?= $language['title'] ?></span></a></li>
+                    <li><a href="<?= $this->getLangUrl($language['code']) ?>" class="<?= $language['code'] === Yii::$app->language ? 'active' : null ?>" data-modal-open data-lang-choose="true"><span><?= $language['title'] ?></span></a></li>
                 <?php } ?>
             </ul>
         </div>
