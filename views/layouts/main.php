02817dad7b8cef84bd81b4d34649e73ee606328d
dmitry_matskevich <dm@gmail.com>
2025-05-17 11:13:05 +0300

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -17,6 +17,41 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
 <head>
+    <?php if ((getenv('ENV') !== 'DEV')) { ?>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG2R4RJ3X5"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-VG2R4RJ3X5');
+        </script>
+        <!-- Google tag (gtag.js)
+
+         Yandex.Metrika counter -->
+        <script data-cfasync="false" type="text/javascript">
+        (function(){
+        var a = function() {try{return !!window.addEventListener} catch(e) {return !1} },
+        b = function(b, c) {a() ? window.addEventListener("load", b, c) : document.attachEvent("onreadystatechange", b)};
+        b(function(){
+
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(XXXXXXXX, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+        });
+
+        }, false);
+        })();
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/98753176" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter -->
+    <?php } ?>
     <title><?= Html::encode($this->title ?? Yii::t('app', 'Школа плавания - Piranha')) ?></title>
     <?= $this->render('_favicon') ?>
     <meta name="msapplication-TileColor" content="#E3007B">
@@ -24,41 +59,6 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
     <meta charset="utf-8">
     <?= $this->initMetaTags() ?>
 
-        <?php if ((getenv('ENV') !== 'DEV')) { ?>
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG2R4RJ3X5"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-VG2R4RJ3X5');
-            </script>
-            <!-- Google tag (gtag.js)
-
-             Yandex.Metrika counter -->
-            <script data-cfasync="false" type="text/javascript">
-            (function(){
-            var a = function() {try{return !!window.addEventListener} catch(e) {return !1} },
-            b = function(b, c) {a() ? document.addEventListener("load", b, c) : document.attachEvent("onreadystatechange", b)};
-            b(function(){
-
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(XXXXXXXX, "init", {
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-            });
-
-            }, false);
-            })();
-            </script>
-            <noscript><div><img src="https://mc.yandex.ru/watch/98753176" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-            <!-- /Yandex.Metrika counter -->
-        <?php } ?>
     <?php $this->head() ?>
 </head>
 
