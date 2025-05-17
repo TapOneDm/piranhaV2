6aaafbf7ba8f10674d9d277980d20a2f8b1d645e
dmitry_matskevich <dm@gmail.com>
2025-05-17 11:10:18 +0300

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -23,17 +23,7 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
     <meta name="theme-color" content="#E3007B">    
     <meta charset="utf-8">
     <?= $this->initMetaTags() ?>
-    <?php $this->head() ?>
-</head>
 
-<body>
-    <?php $this->beginBody() ?>
-    <div class="wrapper">
-        <?= $this->render('header') ?>
-        <?= $content ?>
-        <?= $this->render('_footer') ?>
-        <?= $this->render('../site/_modals') ?>
-    </div>
         <?php if ((getenv('ENV') !== 'DEV')) { ?>
             <script async src="https://www.googletagmanager.com/gtag/js?id=G-VG2R4RJ3X5"></script>
             <script>
@@ -42,7 +32,7 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
                 gtag('js', new Date());
 
                 gtag('config', 'G-VG2R4RJ3X5');
-            </script> -->
+            </script>
             <!-- Google tag (gtag.js)
 
              Yandex.Metrika counter -->
@@ -69,11 +59,21 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
             <noscript><div><img src="https://mc.yandex.ru/watch/98753176" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
             <!-- /Yandex.Metrika counter -->
         <?php } ?>
+    <?php $this->head() ?>
+</head>
 
-    <!-- <div id="overlay">
+<body>
+    <?php $this->beginBody() ?>
+    <div class="wrapper">
+        <?= $this->render('header') ?>
+        <?= $content ?>
+        <?= $this->render('_footer') ?>
+        <?= $this->render('../site/_modals') ?>
+    </div>
+    <div id="overlay">
         <img src="/static/img/logo.svg" alt="logo">
         <span class="loader"></span>
-    </div> -->
+    </div>
     <?php $this->endBody() ?>
 </body>
 
