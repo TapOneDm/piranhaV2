b10a1233f1063036e1d3d2787118b97005a8ac60
dmitry_matskevich <dm@gmail.com>
2025-10-01 22:11:15 +0300

    update share survey

--- views/survey/site/result.php
+++ views/survey/site/result.php
@@ -41,8 +41,9 @@ $imageSrc =  $data['image'][intval($model->question0) - 1];
 
 <script>
     function generateTelegramShareLink(surveyUrl, imageUrl, title, text) {
-        const fullText = encodeURIComponent(title + "\n\n" + imageUrl + "\n\n" + text);
-        const shareUrl = `https://telegram.me/share/url?url=${encodeURIComponent(surveyUrl)}&text=${fullText}`;
+        const fullText = encodeURIComponent(title + "\n\n" + "\n\n" + text);
+        const shareUrl = `https://t.me/share/url?url=${encodeURIComponent(imageUrl)}&text=${fullText}`;
+        // https://t.me/share/url?url=https://piranha.ge/static/img/survey/boy_tech.jpg&text=bobo
         return shareUrl;
     }
 
